0d51b9bc02d1b80490fcbb64cc7a31f26539c906
João Duarte <joaocgarciaduarte@gmail.com>
2021-09-07 02:50:56 +0100

    update(errors): Add `not allowed` error to domain

--- app/domain/errors.go
+++ app/domain/errors.go
@@ -3,6 +3,7 @@ package domain
 import "errors"
 
 var (
+	ErrNotAllowed    = errors.New("not allowed")
 	ErrBadParamInput = errors.New("invalid parameter")
 	ErrNotFound      = errors.New("resource not found")
 	ErrInvalidToken  = errors.New("invalid token")
